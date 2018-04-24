3f2581582e0517fa4ccc18d95cfcbb273529c13a
Yehonatan <yehonatan@yehonatan.yehonatan>
2018-04-24 03:45:37 -0400

    renaming

--- api/login.js
+++ api/login.js
@@ -1,67 +1,67 @@
 module.exports = {
 
-    clickLogin: async function (chrome) {
+    clickLogin: async function (browser) {
         const clickLogin = `document.querySelector('a[href="/accounts/login/"]').click();`;
         // Evaluate the JS expression in the page.
-        let { result } = await chrome.evaluate(clickLogin);
+        let { result } = await browser.evaluate(clickLogin);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    isLoggedIn: async function (username, chrome) {
+    isLoggedIn: async function (username, browser) {
         const isLoggedInHREF = `document.querySelector('a[href="/${username}/"]')`;
         // Evaluate the JS expression in the page.
-        let { result } = await chrome.evaluate(isLoggedInHREF);
+        let { result } = await browser.evaluate(isLoggedInHREF);
         return result.type == 'object' && result.subtype == 'node';
     },
 
-    setLoginDetailsOnLogin: async function (username, password, chrome) {
+    setLoginDetailsOnLogin: async function (username, password, browser) {
         const setUsernameInput = `document.querySelector('input[name="username"]').focus()`;
-        await chrome.evaluate(setUsernameInput);
-        await chrome.inputString(username);
-        await timeout(1000);
+        await browser.evaluate(setUsernameInput);
+        await browser.inputString(username);
+        await sleep(1000);
         const setPasswordInput = `document.querySelector('input[name="password"]').focus()`;
-        var { result } = await chrome.evaluate(setPasswordInput);
-        await chrome.inputString(password);
+        var { result } = await browser.evaluate(setPasswordInput);
+        await browser.inputString(password);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    submitLogin: async function (chrome) {
+    submitLogin: async function (browser) {
         let js = `document.evaluate("//form/span/button[text()='Log in']", document, null, XPathResult.ANY_TYPE,null).iterateNext().click()`;
-        let { result } = await chrome.evaluate(js);
+        let { result } = await browser.evaluate(js);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    submitSuspiciousAttempt: async function (chrome) {
+    submitSuspiciousAttempt: async function (browser) {
         let js = `document.evaluate("//form/span/button[text()='Send Security Code']", document, null, XPathResult.ANY_TYPE,null).iterateNext().click()`;
-        let { result } = await chrome.evaluate(js);
+        let { result } = await browser.evaluate(js);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    isWaitingForCode: async function (chrome) {
+    isWaitingForCode: async function (browser) {
         let js = `document.querySelector('a[href="javascript:_replay()"');`
-        let { result } = await chrome.evaluate(js);
+        let { result } = await browser.evaluate(js);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    inputCode: async function (inputCode, chrome) {
+    inputCode: async function (inputCode, browser) {
         let js = `document.querySelector('input[name="security_code"]').focus();`
-        var { result } = await chrome.evaluate(js);
-        await chrome.inputString(inputCode);
+        var { result } = await browser.evaluate(js);
+        await browser.inputString(inputCode);
         return !(result.type == 'object' && result.subtype == 'error');
     },
 
-    validateSecurityCode: async function (chrome) {
+    validateSecurityCode: async function (browser) {
         let js = `document.evaluate("//form/span/button[text()='Submit']", document, null, XPathResult.ANY_TYPE,null).iterateNext().click()`;
-        let { result } = await chrome.evaluate(js);
+        let { result } = await browser.evaluate(js);
         return !(result.type == 'object' && result.subtype == 'error');
     },
-    hasSuspiciousLoginAttempt: async function (chrome) {
+    hasSuspiciousLoginAttempt: async function (browser) {
         let js = `document.evaluate("//p[text()='Suspicious Login Attempt']", document, null, XPathResult.ANY_TYPE,null).iterateNext().innerHTML`;
-        let { result } = await chrome.evaluate(js);
+        let { result } = await browser.evaluate(js);
         return result.type == 'string' && result.value == 'Suspicious Login Attempt';
     }
 }
 
-function timeout(ms) {
+function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
