e868383e5fbfc6eea557d270904e2bae0c89c770
jonathan <y@tsirolnik.me>
2018-05-28 21:55:02 +0300

    unfollow, formatting

--- api/user.js
+++ api/user.js
@@ -1,20 +1,27 @@
 module.exports = {
 
-    getFollowersNumber: async function(browser) {
+    getFollowersNumber: async function (browser) {
         let nodes = await browser.getNodesByXPath(`//span[@class="_fd86t "]/text()`);
+        if (nodes.length === 0) {
+            throw new Error('Could not get user total followers');
+        }
         let followersString = nodes[1].value;
-        let followers = Number(followersString.replace(/,/g,''));
+        let followers = Number(followersString.replace(/,/g, ''));
         return followers;
     },
 
-    getUsername : async function(browser) {
-        let {result} = await browser.evaluate(`document.querySelector('h1').innerText`);
-        if(!result.type === 'string') throw new Error('Could not find username');
+    getUsername: async function (browser) {
+        let { result } = await browser.evaluate(`document.querySelector('h1').innerText`);
+        if (!result.type === 'string') throw new Error('Could not find username');
         return result.value;
     },
 
-    follow: async function(browser) {
+    follow: async function (browser) {
         await browser.evaluate(`document.querySelector('._qv64e._gexxb._r9b8f._njrw0').click();`);
+    },
+
+    unfollow: async function (browser) {
+        await browser.evaluate(`cument.querySelector('._qv64e._t78yp._r9b8f._njrw0').click();`);
     }
 
 }
