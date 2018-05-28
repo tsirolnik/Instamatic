ae97206cce3efd530a3464814ac90d2c34072fb2
jonathan <y@tsirolnik.me>
2018-05-28 21:54:23 +0300

    add logger to user

--- api/post.js
+++ api/post.js
@@ -4,7 +4,7 @@ module.exports = {
         if (!result.type === 'string') throw new Error('Could not find username');
         return result.value;
     },
-    userProfile: async function(browser) {
+    userProfile: async function(browser, logger) {
         let { result } = await browser.evaluate(`document.querySelector('._2g7d5.notranslate._iadoq').href`);
         if (result.type != 'string') {
             logger.error('Could not find profile link', { result })
