c913bded14e401426c9ff40ac42383ca69d3d09e
jonathan <y@tsirolnik.me>
2018-06-05 16:45:18 +0300

    constant added

--- api/post.js
+++ api/post.js
@@ -1,11 +1,13 @@
+const POST_SELECTOR_USERNAME = '.FPmhX.notranslate.nJAzx';
+
 module.exports = {
     username: async function (browser) {
-        let { result } = await browser.evaluate(`document.querySelector('._2g7d5.notranslate._iadoq').text`);
+        let { result } = await browser.evaluate(`document.querySelector('${POST_SELECTOR_USERNAME}').text`);
         if (!result.type === 'string') throw new Error('Could not find username');
         return result.value;
     },
     userProfile: async function(browser, logger) {
-        let { result } = await browser.evaluate(`document.querySelector('._2g7d5.notranslate._iadoq').href`);
+        let { result } = await browser.evaluate(`document.querySelector('${POST_SELECTOR_USERNAME}').href`);
         if (result.type != 'string') {
             logger.error('Could not find profile link', { result })
             return '';
