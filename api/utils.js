294aadcb9d9a7d8dd3688bea55e40965f15462fb
jonathan <y@tsirolnik.me>
2018-06-05 16:47:36 +0300

    move sleep to utils, fix rand range

--- api/utils.js
+++ api/utils.js
@@ -1,3 +1,11 @@
+const winston = require('winston');
+const logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)()
+    ],
+    exitOnError: false
+});
+
 module.exports = {
     checkChance: function (chance) {
         let randNum = Math.floor(Math.random() * 100) + 1;
@@ -7,7 +15,17 @@ module.exports = {
         return false;
     },
 
-    randRange: function(min, max) {
-        return Math.floor(Math.random() * max) + min;
+    randRange: function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    sleep: function(ms) {
+        logger.info('Sleeping', { ms, now: Date.now() });
+        return new Promise(resolve => {
+            setTimeout(() => {
+                logger.info('Slept', { ms, now: Date.now() });
+                resolve();
+            }, ms);
+        });
     }
 }
